42c433e977b9011136db8891af102e88a29ad563
skufmid <skufmid@gmail.com>
2023-01-27 16:41:27 +0900

    사진 크기 300으로 수정

--- lunch_bot.py
+++ lunch_bot.py
@@ -18,8 +18,8 @@ for num in range(5):
     thumb_url = data['thumbnailUrl']
 
     print(f'**{num+1}. {corner}**')
-    print(f'#### {name}')
+    print(f'##### {name}')
     print(side)
     # print(f'탄수화물 {carb}g  단백질 {protein}g  지방 {fat}g  나트륨 {salt}mg')
     print(f'{kcal}kcal')
-    print(f'![{name}]({thumb_url} =250)')
+    print(f'![{name}]({thumb_url} =300)')
