3e68e0d8bdd97de1083a6a0b1143894c4063d5ef
skufmid <skufmid@gmail.com>
2023-02-15 11:09:25 +0900

    11시 20분

--- lunch_bot_auto.py
+++ lunch_bot_auto.py
@@ -47,7 +47,7 @@ def dailyMenuUpdate():
     dailyUpdateAndPost(url_hook)
 
 if __name__ == '__main__':
-    localtime = "02:10"
+    localtime = "02:20"
     schedule.every().monday.at(localtime).do(dailyMenuUpdate)
     schedule.every().tuesday.at(localtime).do(dailyMenuUpdate)
     schedule.every().wednesday.at(localtime).do(dailyMenuUpdate)
